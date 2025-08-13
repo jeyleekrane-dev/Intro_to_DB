9e04a697ca8b44f577c7dc12eae6959c3c5521ec
jeyleekrave-dev <jeyleekrane@gmail.com>
2025-08-14 00:00:10 +0100

    alx_book modified

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,42 +6,42 @@ USE alx_book_store;
 
 -- Create the 'Authors' table
 CREATE TABLE Authors (
-    author_id INT PRIMARY KEY,
-    author_name VARCHAR(215)
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
 );
 
 -- Create the 'Books' table
 CREATE TABLE Books (
-    book_id INT PRIMARY KEY,
-    title VARCHAR(130),
-    author_id INT,
-    price DOUBLE,
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
     publication_date DATE,
     FOREIGN KEY(author_id) REFERENCES Authors(author_id)
 );
 
 -- Create the 'Customers' table
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
 -- Create the 'Orders' table
 CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
     FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create the 'Order_Details' table
 CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE,
+    order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity INT NOT NULL,
     FOREIGN KEY(order_id) REFERENCES Orders(order_id),
     FOREIGN KEY(book_id) REFERENCES Books(book_id)
 );
